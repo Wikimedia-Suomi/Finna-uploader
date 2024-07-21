355dd9ae22a7bdf29c66d62c2ef17f7b59638857
Ilkka Prusi <ilkka.prusi@gmail.com>
2024-07-21 19:30:12 +0300

    comments

--- finnauploader/finnauploader/apps.py
+++ finnauploader/finnauploader/apps.py
@@ -1,11 +1,13 @@
 from django.apps import AppConfig
 
-
+# this is defined in __init__.py, change it if you rename this
 class MyAppConfig(AppConfig):
 
     name = 'finnauploader'
     verbose_name = "My Application"
 
+    # this is called when "registry" has been initialized and may called more than once?
+    # also called once on each command.
     def ready(self):
         print("apps.py ready()")
         try:
@@ -31,3 +33,4 @@ class MyAppConfig(AppConfig):
             SubjectPlacesCache.objects.update()
         except:
             pass
+
