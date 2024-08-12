858be72bb4141e3581fad4649208e39d3c5bfa4a
Ilkka Prusi <ilkka.prusi@gmail.com>
2024-08-12 19:18:39 +0300

    clarify, aid debugging, more intelligent caching

--- finnauploader/images/models_mappingcache.py
+++ finnauploader/images/models_mappingcache.py
@@ -18,6 +18,7 @@ class FinnaMappingsCacheManager(models.Manager):
 
     def update(self):
         page_title = self.model.page_title
+        print("Saving cache for:", page_title)
 
         site = pywikibot.Site("commons")
 
@@ -33,7 +34,8 @@ class FinnaMappingsCacheManager(models.Manager):
         # Test if there is newer revisions in wiki
         update = int(obj.rev_id) < rev_id
 
-        # Update cache if needed
+        # Update cache if needed:
+        # commons page revision has changed or local cache is being initialized
         if created or update:
             print(f'Updating {page_title}')
             self._update_cache(site, page_title, rev_id, obj)
@@ -41,27 +43,41 @@ class FinnaMappingsCacheManager(models.Manager):
     def _get_latest_revision_id(self, site, page_title):
         rev_id = 0
         for n in range(1, 5):
-            new_page_title = page_title if n == 1 else f'{page_title}_{n}'
+            if n == 1:
+                new_page_title = self.page_title
+            else:
+                new_page_title = f'{self.page_title}_{n}'
             page = pywikibot.Page(site, new_page_title)
             if page.exists() and page.latest_revision_id > rev_id:
                 rev_id = page.latest_revision_id
         return rev_id
 
     def _update_cache(self, site, page_title, rev_id, obj):
+        print("Saving cache for:", page_title)
         cache = MappingCache()
         rows = cache.parse_cache_page(site, page_title)
+        
+        # TODO: see below, you should clear entire cache every time
         self.clear()
+
         for name in rows:
             wikidata_id = rows[name]
             self.get_or_create(name=name, wikidata_id=wikidata_id)
+            
+            # TODO: this needs testing so we don't need to rewrite everything each time:
+            # just update changes
+            #o, created = self.get_or_create(name=name, wikidata_id=wikidata_id)
+            #if (created == True):
+                #print("DEBUG: added to cache:", name, ":", wikidata_id)
         obj.rev_id = rev_id
         obj.save()
+        print("Saved cache for:", page_title)
 
 
 class FinnaMappingsCache(models.Model):
     objects = FinnaMappingsCacheManager()
 
-    page_title = 'User:FinnaUploadBot/data/nonPresenterAuthors'
+    page_title = ''
     name = models.CharField(max_length=255)
     wikidata_id = models.CharField(max_length=32)
 
