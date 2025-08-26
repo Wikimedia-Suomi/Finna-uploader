fb13a04b81f3ba4ada5f9ca8c908d7ced0cb7ad7
Ilkka Prusi <ilkka.prusi@gmail.com>
2025-08-27 02:27:15 +0300

    split loop, catch if file doesn't exist any more

--- finnauploader/images/management/commands/set_finna_id_from_externallinks_to_image.py
+++ finnauploader/images/management/commands/set_finna_id_from_externallinks_to_image.py
@@ -8,11 +8,24 @@ import requests
 
 class Command(BaseCommand):
     help = 'Set Image.finna_id based on Finna_id from externallinks'
+    
+    # images might have been renamed in commons -> don't crash
+    #
+    def confirm_image(self, site, finna_id, page_title):
+        if not finna_id:
+            return None
+        
+        commons_thumbnail_url = None
+        try:
+            file_page = pywikibot.FilePage(site, page_title)
+            commons_thumbnail_url = file_page.get_file_url(url_width=1000)
+        except NoPageError:
+            print("page missing from commons: ", page_title)
+            return None
+            
+        return is_correct_finna_record(finna_id, commons_thumbnail_url)
 
-    def handle(self, *args, **kwargs):
-        site = pywikibot.Site("commons", "commons")  # for Wikimedia Commons
-        site.login()
-
+    def confirm_images(self, site):
         images = Image.objects.filter(finna_id__isnull=False)
         number_of_images=images.count()
         print(f'Images to do {number_of_images}')
@@ -24,16 +37,21 @@ class Command(BaseCommand):
             for url in image.urls.all():
                 print(url.url)
                 finna_id=get_finna_id_from_url(url.url)
-                if finna_id:
-                    file_page = pywikibot.FilePage(site, image.page_title)
-                    commons_thumbnail_url=file_page.get_file_url(url_width=1000)
-                    confirmed_finna_id=is_correct_finna_record(finna_id, commons_thumbnail_url)
+                
+                confirmed_finna_id = self.confirm_image(site, finna_id, image.page_title)
+                if confirmed_finna_id:
                     print(confirmed_finna_id)
-                    if confirmed_finna_id:
-                        image.finna_id = confirmed_finna_id
-                        image.finna_id_confirmed = True
-                        image.save()
-                        print("saving")
-                        break                
+                    image.finna_id = confirmed_finna_id
+                    image.finna_id_confirmed = True
+                    image.save()
+                    print("saving")
+                    break
+        
+
+    def handle(self, *args, **kwargs):
+        site = pywikibot.Site("commons", "commons")  # for Wikimedia Commons
+        site.login()
+        
+        self.confirm_images(site)
 
         self.stdout.write(self.style.SUCCESS(f'Finna_ids updated successfully!'))
